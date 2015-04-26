e1aa746701a2e31479d0f9f4fa5fe0c48c7cd01d
cuizheng <zheng.cuizh@gmail.com>
2015-04-26 21:05:14 +0800

    先设置uuid之后再建立uuid的uniq索引

--- db/migrate/20150425165631_add_uuid_to_eye.rb
+++ db/migrate/20150425165631_add_uuid_to_eye.rb
@@ -2,8 +2,8 @@ class AddUuidToEye < ActiveRecord::Migration
   def change
     add_column :eyes, :name, :string
     add_column :eyes, :uuid, :string, :null => false
-    add_index :eyes, :uuid, unique: true
 
     Eye.all.each { |e| e.uuid = UUID.timestamp_create().to_s; e.save }
+    add_index :eyes, :uuid, unique: true
   end
 end
